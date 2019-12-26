91f0d621c868436c90d58564fadeccfae1da94dd
ankush <ankush@gmail.com>
2019-12-26 15:48:03 +0530

    Add project files.

---
+++ BarcodeLib/Controllers/BarcodeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace gotoconfig.BarcodeLib.Controllers
+{
+    public class BarcodeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
---
+++ BarcodeLib/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DefaultMVCApp.MvcApplication" Language="C#" %>
